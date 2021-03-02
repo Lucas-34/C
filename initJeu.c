28b87ee1e049d08b0000adc1ef42496201d1c102
Lucas <lucas.ben-djedidia@etu.umontpellier.fr>
2021-03-02 21:30:36 +0100

    test 2

--- initJeu.c
+++ initJeu.c
@@ -6,32 +6,30 @@
 #include <sys/shm.h>
 #include <stdlib.h>
 
-
-
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   if (argc != 3)
   {
-    printf("Le nombre d'arguments est invalide. Utilisation : \n");  
-    printf("%s fichier-pour-cle-ipc nbre-de-joueurs_attendus \n", argv[0]);    
+    printf("Le nombre d'arguments est invalide. Utilisation : \n");
+    printf("%s fichier-pour-cle-ipc nbre-de-joueurs_attendus \n", argv[0]);
     exit(0);
   }
-	  
-  int cleSem = ftok(argv[1], 'r');
-  
-  int idSem = semget(cleSem, 1, IPC_CREAT|0600); // 1 : nombre de sémaphores créés
 
-  if(idSem == -1)
+  int cleSem = ftok(argv[1], 'r');
+
+  int idSem = semget(cleSem, 1, IPC_CREAT | 0600); // 1 : nombre de sémaphores créés
+
+  if (idSem == -1)
   {
     perror("Echec de l'exécution de la fonction semget");
     exit(-1);
   }
 
-  struct sembuf op[1];  // structure pour stocker l'opération sur le sémaphore 0
+  struct sembuf op[1]; // structure pour stocker l'opération sur le sémaphore 0
 
-  op[0].sem_num = 0;  // n° de sémaphore
-  op[0].sem_op = -1 * ((short) atoi(argv[2]));    // valeur initiale du sémaphore = - nombre de joueurs saisis
-  op[0].sem_flg = 0;  // flag
+  op[0].sem_num = 0;                          // n° de sémaphore
+  op[0].sem_op = -1 * ((short)atoi(argv[2])); // valeur initiale du sémaphore = - nombre de joueurs saisis
+  op[0].sem_flg = 0;                          // flag
 
   semop(idSem, op, 1);
 
@@ -41,4 +39,4 @@ int main(int argc, char * argv[])
 
   return 0;
 }
-
+00000
