fdab75cd378c45eaababd922ee93337b0e880da6
Lucas <lucas.ben-djedidia@etu.umontpellier.fr>
2021-03-04 21:17:51 +0100

    exo2 corrigé

--- tpipc/exo2/initJeu.c
+++ tpipc/exo2/initJeu.c
@@ -6,40 +6,45 @@
 #include <sys/shm.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   if (argc != 3)
   {
-    printf("Les arguments sont invalides. Les arguments doivent être: '%s fichier_de_référence nombre_de_joueurs_attendus'. \n", argv[0]);
+    printf("initJeu : Les arguments sont invalides. Ils doivent être: '%s fichier_de_référence nombre_de_joueurs_attendus'. \n", argv[0]);
     
     exit(0);
   }
 
-  key_t cle = ftok(argv[1], 'r');
+  key_t cle = ftok(argv[1], 'r'); // On crée la clé.
 
   int idSem = semget(cle, 1, IPC_CREAT | 0600); // Le 2nd paramètre correspond au nombre de sémaphores présents dans notre tableau de sémaphores. Il y a donc ici 1 sémaphore.
 
   if (idSem == -1)
   {
-    perror("Echec de l'exécution de la fonction semget");
-    
+    perror("initJeu : Echec de l'exécution de la fonction semget");
     exit(-1);
   }
 
-  struct sembuf operation[1]; // "sembuf" est le type 'opération sur un sémaphore'. Un sémaphore est une structure de données qui comprend notamment comme attribut un compteur (entier) correspondant au nombre de process pouvant passer en même temps dans la section critique. Une opération sur un sémaphore est plus précisément une opération sur son compteur: il s'agit d'incrémenter ou décrémenter le compteur. On définit par ailleurs 3 catégories d'opérations: V pour l'incrémentation (valeur positive), P pour la décrémentation (valeur négative), et Z qui est un peu spéciale car elle correspond à la valeur 0 (donc le compteur ne change pas) mais fait aussi que chaque process qui exécutera cette opération sera bloqué du moment que le compteur est différent de 0. Ici, "operation" est un tableau d'1 opération.
+  struct sembuf operation[1]; // "sembuf" est le type 'opération sur un sémaphore'. Un sémaphore est une structure de données qui comprend notamment comme attribut un compteur (entier) correspondant au nombre de process pouvant passer en même temps dans la section critique. Une opération sur un sémaphore est plus précisément une opération sur son compteur: il s'agit d'incrémenter ou de décrémenter le compteur. On définit par ailleurs 3 catégories d'opérations: V pour l'incrémentation (valeur positive), P pour la décrémentation (valeur négative), et Z qui est un peu spéciale car elle correspond à la valeur 0 (donc le compteur ne change pas) mais fait aussi que chaque process qui exécutera cette opération sera bloqué du moment que le compteur est différent de 0. Ici, "operation" est un tableau d'1 opération.
 
   // On initialise l'unique opération:
-
   operation[0].sem_num = 0; // Les numéros des sémaphores ne correspondent pas à leur indice dans le tableau de sémaphores. Ces numéros commencent à 0.
-  operation[0].sem_op = -1 * ((short) atoi(argv[2])); // Initialement, le compteur d'un sémaphore vaut 0. On va donc lui ajouter (- nombre_de_joueurs_attendus) (valeur affectée à sem_op qui correspond à la valeur qui est ajoutée au compteur). Ainsi, à chaque fois qu'un process arrivera sur la section critique, on incrémentera le compteur de 1 (opération V avec un sem_op = 1) et on les bloquera (opération Z) tel que lorsque le compteur aura atteint 0, cela voudra dire que tous les process auront été réunis au même endroit (là on exécute ces 2 opérations).
+  operation[0].sem_op = (short) atoi(argv[2]); // Initialement, le compteur d'un sémaphore vaut 0. On va donc lui ajouter ( nombre_de_joueurs_attendus) (valeur affectée à sem_op qui correspond à la valeur qui est ajoutée au compteur). Ainsi, à chaque fois qu'un process arrivera sur la section critique, on incrémentera le compteur de 1 (opération V avec un sem_op = 1) et on les bloquera (opération Z) tel que lorsque le compteur aura atteint 0, cela voudra dire que tous les process auront été réunis au même endroit (là on exécute ces 2 opérations).
   operation[0].sem_flg = 0; // Option de l'opération
 
+  semop(idSem, operation, 1); // La fonction "semop" permet d'exécuter l'opération dont la syntaxe avait été définie précédemment. Dans le cas où sem_op est négatif, semop bloque les process qui l'exécuteront. Dans le cas où sem_op est nul, semop bloque les process qui l'exécutent tant que le compteur est différent de 0.
+
+  printf("initJeu : La valeur du compteur à ce moment est %d. \n", semctl(idSem, 0, GETVAL)); // Le 2nd paramètre est le numéro du sémaphore. Il n'y en a qu'1 ici, de numéro 0.
+  fflush(stdout); // Permet de forcer l'écriture sur la sortie standard.
+
+
+ // On définit une opération Z pour bloquer le process jusqu'à ce que le compteur soit à 0:
+  operation[0].sem_num = 0;
+  operation[0].sem_op = 0;
+  operation[0].sem_flg = 0;
   semop(idSem, operation, 1);
 
-  printf("La valeur du compteur est %d. \n", semctl(idSem, 0, GETVAL)); // Le 2nd paramètre est le numéro du sémaphore. Il n'y en a qu'1 ici, de numéro 0.
-  
-  printf("bloqué\n");
-  
+  printf("initJeu : débloqué aussi !");
   fflush(stdout);
 
   return 0;
--- tpipc/exo2/joueur.c
+++ tpipc/exo2/joueur.c
@@ -6,45 +6,56 @@
 #include <sys/shm.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
   if (argc != 2)
   {
-    printf("Le nombre d'arguments est invalide. Utilisation : \n");
-    printf("%s fichier-pour-cle-ipc \n", argv[0]);
+    printf("Les arguments sont invalides. Ils doivent être: '%s fichier_de_référence. \n", argv[0]);
+    
     exit(0);
   }
 
-  int cleSem = ftok(argv[1], 'r');
+  key_t cle = ftok(argv[1], 'r'); // On re-crée la même clé que dans le programme initJeu car les paramètres sont identiques.
 
-  int idSem = semget(cleSem, 1, IPC_CREAT | 0600); // 1 : nombre de sémaphores créés
+  int idSem = semget(cle, 1, IPC_CREAT | 0600); // On re-crée le même identifiant. Comme on a accès au même objet IPC, on aura accès au même sémaphore de numéro 0.
 
-  if (idSem == -1)
+  if(idSem == -1)
   {
-    perror("Echec de l'exécution de la fonction semget");
+    perror("joueur : Echec de l'exécution de la fonction semget."); 
     exit(-1);
   }
 
-  struct sembuf op[1]; // structure pour stocker l'opération sur le sémaphore 0
+  struct sembuf operation[1];
 
-  // V(1) pour incrémenter le sémaphore de rdv
-  op[0].sem_num = 0; // n° de sémaphore
-  op[0].sem_op = 1;  // chaque joueur incrémente le sémaphore : V(1)
-  op[0].sem_flg = 0; // flag
-  semop(idSem, op, 1);
+  // On définit une opération V (associée à la valeur 1):
+  operation[0].sem_num = 0;
+  operation[0].sem_op = -1; // A l'endroit où les process (joueurs) doivent se réunir, chacun d'entre eux incrémentera le compteur du sémaphore de 1.
+  operation[0].sem_flg = 0;
+  
+  if(semop(idSem, operation, 1) == -1)
+  {
+    perror("joueur : Echec de l'exécution de la fonction semop.");
+    exit(-1);
+  }
 
-  printf("valeur du sémaphore avant blocage : %d\n", semctl(idSem, 0, GETVAL)); // 0 est le n° sémaphore
-  printf("bloqué\n");
+  printf("joueur : Le compteur du sémaphore avant le blocage du process courant est %d. \n", semctl(idSem, 0, GETVAL));
   fflush(stdout);
-  // Z pour bloquer jusqu'à ce que le sémaphore soit à 0
-  op[0].sem_num = 0; // n° de sémaphore
-  op[0].sem_op = 0;  // Z(0) : bloqué en attendant que le sémaphore soit revenu à 0
-  op[0].sem_flg = 0; // flag à 0
-  semop(idSem, op, 1);
-
-  printf("débloqué !\n");
-  printf("valeur du sémaphore après déblocage : %d\n", semctl(idSem, 0, GETVAL)); // 0 est le n° sémaphore
+  printf("joueur : Le process courant est bloqué. \n");
   fflush(stdout);
+
+  // On définit une opération Z pour bloquer les process jusqu'à ce que le compteur soit à 0:
+  operation[0].sem_num = 0;
+  operation[0].sem_op = 0;
+  operation[0].sem_flg = 0;
+  
+  semop(idSem, operation, 1);
+
+  printf("joueur : Process débloqué ! \n");
+  fflush(stdout);
+  printf("joueur : Le compteur après le déblocage des process vaut %d. \n", semctl(idSem, 0, GETVAL));
+  fflush(stdout);
+
+  semctl(idSem, 0, IPC_RMID); // On détruit le sémaphore car il ne nous sert plus.
 
   return 0;
 }
