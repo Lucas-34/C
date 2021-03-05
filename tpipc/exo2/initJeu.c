3b937ac88ae5b4dffdb6111617025e359fb45076
Lucas <lucas.ben-djedidia@etu.umontpellier.fr>
2021-03-05 09:46:46 +0100

    nouveaux commentaires

--- tpipc/exo2/initJeu.c
+++ tpipc/exo2/initJeu.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 {
   if (argc != 3)
   {
-    printf("initJeu : Les arguments sont invalides. Ils doivent être: '%s fichier_de_référence nombre_de_joueurs_attendus'. \n", argv[0]);
+    printf("initJeu: Les arguments sont invalides. Ils doivent être: '%s fichier_de_référence nombre_de_joueurs_attendus'. \n", argv[0]);
 
     exit(0);
   }
@@ -21,29 +21,47 @@ int main(int argc, char *argv[])
 
   if (idSem == -1)
   {
-    perror("initJeu : Echec de l'exécution de la fonction semget");
+    perror("initJeu: Echec de l'exécution de la fonction semget");
+    
     exit(-1);
   }
 
-  struct sembuf operation[1]; // "sembuf" est le type 'opération sur un sémaphore'. Un sémaphore est une structure de données qui comprend notamment comme attribut un compteur (entier) correspondant au nombre de process pouvant passer en même temps dans la section critique. Une opération sur un sémaphore est plus précisément une opération sur son compteur: il s'agit d'incrémenter ou de décrémenter le compteur. On définit par ailleurs 3 catégories d'opérations: V pour l'incrémentation (valeur positive), P pour la décrémentation (valeur négative), et Z qui est un peu spéciale car elle correspond à la valeur 0 (donc le compteur ne change pas) mais fait aussi que chaque process qui exécutera cette opération sera bloqué du moment que le compteur est différent de 0. Ici, "operation" est un tableau d'1 opération.
+  struct sembuf operation[1]; // "sembuf" est le type 'opération sur un sémaphore'. Un sémaphore est une structure de données qui comprend notamment comme attribut un compteur (entier) correspondant au nombre de places disponibles dans la section critique (un process pouvant prendre 0, 1 ou plusieurs place(s)). Une opération sur un sémaphore est répartie selon 3 catégories: V (valeur positive, qui correspond au nombre de places supplémentaires) consiste à incrémenter le compteur (=> le nombre de places disponibles augmente donc); P (valeur négative qui correspond, en valeur absolue, au nombre de places demandées par le process qui exécute l'opération pour entrer dans la section critique) consiste à ajouter cette valeur au compteur si elle lui est <= en valeur absolue (=> le compteur diminue car on a pris ces places), sinon à laisser le compteur inchangé et à bloquer le process qui exécute l'opération (puisque le nombre de places disponibles est insuffisant, on accepte pas ce process); et enfin l'opération Z (valeur nulle) qui bloque tout process exécutant cette opération du moment que le compteur est différent de 0. Ici, "operation" est un tableau d'1 opération.
 
   // On initialise l'unique opération:
-  operation[0].sem_num = 0;                    // Les numéros des sémaphores ne correspondent pas à leur indice dans le tableau de sémaphores. Ces numéros commencent à 0.
+  operation[0].sem_num = 0; // Les numéros des sémaphores ne correspondent pas à leur indice dans le tableau de sémaphores. Ces numéros commencent à 0.
   operation[0].sem_op = (short)atoi(argv[2]); // Initialement, le compteur d'un sémaphore vaut 0. On va donc lui ajouter ( nombre_de_joueurs_attendus) (valeur affectée à sem_op qui correspond à la valeur qui est ajoutée au compteur). Ainsi, à chaque fois qu'un process arrivera sur la section critique, on incrémentera le compteur de 1 (opération V avec un sem_op = 1) et on les bloquera (opération Z) tel que lorsque le compteur aura atteint 0, cela voudra dire que tous les process auront été réunis au même endroit (là on exécute ces 2 opérations).
-  operation[0].sem_flg = 0;                    // Option de l'opération
+  operation[0].sem_flg = 0; // Option de l'opération
 
-  semop(idSem, operation, 1); // La fonction "semop" permet d'exécuter l'opération dont la syntaxe avait été définie précédemment. Dans le cas où sem_op est négatif, semop bloque les process qui l'exécuteront. Dans le cas où sem_op est nul, semop bloque les process qui l'exécutent tant que le compteur est différent de 0.
+  semop(idSem, operation, 1); // La fonction "semop" permet d'exécuter l'opération dont la syntaxe avait été définie précédemment.
 
-  printf("initJeu : La valeur du compteur à ce moment est %d. \n", semctl(idSem, 0, GETVAL)); // Le 2nd paramètre est le numéro du sémaphore. Il n'y en a qu'1 ici, de numéro 0.
-  fflush(stdout);                                                                             // Permet de forcer l'écriture sur la sortie standard.
+  if(semop(idSem, operation, 1) == -1)
+  {
+    perror("Joueur: Echec de l'exécution de la fonction semop.");
+    
+    exit(-1);
+  }
 
-  // On définit une opération Z pour bloquer le process jusqu'à ce que le compteur soit à 0. Il attend que les joueurs le débloque aussi
+  printf("initJeu: La valeur du compteur à ce moment est %d. \n", semctl(idSem, 0, GETVAL)); // Le 2nd paramètre est le numéro du sémaphore. Il n'y en a qu'1 ici, de numéro 0.
+  
+  fflush(stdout); // Permet de forcer l'écriture sur la sortie standard.
+
+  // On définit une opération Z pour bloquer initJeu jusqu'à ce que le compteur soit à 0 : ce sont les joueurs qui vont le débloquer. Il attend que les joueurs le débloque aussi
   operation[0].sem_num = 0;
   operation[0].sem_op = 0;
   operation[0].sem_flg = 0;
+  
   semop(idSem, operation, 1);
 
-  printf("initJeu : débloqué aussi !");
+  if(semop(idSem, operation, 1) == -1)
+  {
+    perror("Joueur: Echec de l'exécution de la fonction semop.");
+    
+    exit(-1);
+  }
+
+  printf("initJeu: débloqué aussi !");
+  
   fflush(stdout);
 
   semctl(idSem, 0, IPC_RMID); // On détruit le sémaphore car il ne nous sert plus.
--- tpipc/exo2/joueur.c
+++ tpipc/exo2/joueur.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 {
   if (argc != 2)
   {
-    printf("Les arguments sont invalides. Ils doivent être: '%s fichier_de_référence. \n", argv[0]);
+    printf("Joueur: Les arguments sont invalides. Ils doivent être: '%s fichier_de_référence. \n", argv[0]);
     
     exit(0);
   }
@@ -21,7 +21,8 @@ int main(int argc, char* argv[])
 
   if(idSem == -1)
   {
-    perror("joueur : Echec de l'exécution de la fonction semget."); 
+    perror("Joueur: Echec de l'exécution de la fonction semget."); 
+    
     exit(-1);
   }
 
@@ -29,18 +30,24 @@ int main(int argc, char* argv[])
 
   // On définit une opération P (associée à la valeur -1):
   operation[0].sem_num = 0;
-  operation[0].sem_op = -1; // A l'endroit où les process (joueurs) doivent se réunir, chacun d'entre eux incrémentera le compteur du sémaphore de 1.
+  operation[0].sem_op = -1; // A l'endroit où les process (joueurs) doivent se réunir, chacun d'entre eux décrémentera le compteur du sémaphore de 1 et entrera dans la section critique ssi |-1| = 1 <= nombre_de_joueurs_attendus, sinon sera bloqué et le compteur reste inchangé.
   operation[0].sem_flg = 0;
+  
+  semop(idSem, operation, 1);
   
   if(semop(idSem, operation, 1) == -1)
   {
-    perror("joueur : Echec de l'exécution de la fonction semop.");
+    perror("Joueur: Echec de l'exécution de la fonction semop.");
+    
     exit(-1);
   }
 
-  printf("joueur : Le compteur du sémaphore avant le blocage du process courant est %d. \n", semctl(idSem, 0, GETVAL));
+  printf("Joueur: Le compteur du sémaphore avant le blocage du process courant est %d. \n", semctl(idSem, 0, GETVAL));
+  
   fflush(stdout);
-  printf("joueur : Le process courant est bloqué. \n");
+  
+  printf("Joueur: Le process courant est bloqué. \n");
+  
   fflush(stdout);
 
   // On définit une opération Z pour bloquer les process jusqu'à ce que le compteur soit à 0:
@@ -50,9 +57,19 @@ int main(int argc, char* argv[])
   
   semop(idSem, operation, 1);
 
-  printf("joueur : Process débloqué ! \n");
+  if(semop(idSem, operation, 1) == -1)
+  {
+    perror("Joueur: Echec de l'exécution de la fonction semop.");
+    
+    exit(-1);
+  }
+
+  printf("Joueur: Le compteur a atteint 0. Tous les process sont débloqués ! \n");
+  
   fflush(stdout);
-  printf("joueur : Le compteur après le déblocage des process vaut %d. \n", semctl(idSem, 0, GETVAL));
+  
+  printf("Joueur: Le compteur après le déblocage des process vaut %d. \n", semctl(idSem, 0, GETVAL));
+  
   fflush(stdout);
 
   return 0;
